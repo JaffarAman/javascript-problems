2ca4307f0f17a53dc0af2c5c279574dfb2d83fd0
Jaffar Aman <amanjaffar50@gmail.com>
2021-03-16 15:23:52 +0500

    work complete

--- app.js
+++ app.js
@@ -316,6 +316,364 @@
 // document.write( ` You will need ${maxAge * maxSnack} to last you until the ripe old age of ${maxAge}`)   
 
 
+
+
+
+/////============ CHAPTER NO 06  -  09 ========/////
+
+/////=============Question # 01 ======//////
+
+// var a = 10 ; 
+// document.write(` Result: <br>
+//                 The value of a is : ${a} <br>
+//                 <br> <br>
+//                 .................................... <br> <br>
+//                 The value if ++a is: ${++a} <br>
+//                 Now the value  of a is: ${a}
+//                 <br> <br>
+//                 The value if a++ is: ${a++} <br>
+//                 Now the value  of a is: ${a}
+//                 <br> <br>
+//                 The value if --a is: ${--a} <br>
+//                 Now the value  of a is: ${a}
+//                 <br> <br>
+//                 The value if a-- is: ${a--} <br>
+//                 Now the value  of a is: ${a}
+
+//                 `)
+
+
+/////=============Question # 02 ======//////
+// var a = 2;
+// var b = 1;
+// var result = --a - --b  + ++b + b;
+//             // 1    -  0   +  1  + 1
+// document.write(` a is ${a} <br> 
+//                 b is ${b} <br>
+//                 result is ${result}
+//                 `)
+
+
+/////=============Question # 03 ======//////
+
+// var userName = prompt("Enter your name");
+// alert("Hello " + userName)    
+
+/////=============Question # 04 - 05  ======//////
+
+// var tableNum = prompt("please enter a table number");
+// if (tableNum !== "") {
+//     for (var i = 1; i <= 10; i++) {
+//         document.write(`${tableNum} x ${i} = ${tableNum * i} <br>`)
+//     }
+// }
+// else {
+//     for (var i = 1; i <= 10; i++) {
+//         document.write(`5 x ${i} = ${5 * i} <br>`)
+//     }
+// }
+
+
+/////=============Question # 06 ======//////
+
+// var sub1 = prompt("Enter subject 1 name");
+// var sub2 = prompt("Enter subject 2 name");
+// var sub3 = prompt("Enter subject 3 name");
+// var totalMark = 100;
+// var obt1 = parseInt(prompt("Enter OBT. Marks subject 1 "))
+// var obt2 = parseInt(prompt("Enter OBT. Marks subject 2 "))
+// var obt3 = parseInt(prompt("Enter OBT. Marks subject 3 "))
+// var totalmarks = obt1 + obt2 + obt3;
+// var per = totalmarks / 300 * 100;
+// // per.toFixed()
+
+// document.write("<table>")
+// document.write("<tr>")
+// document.write("<td> " + "<b>"+ "Subject"  + "</b>" + "</td>" +  
+//                 "<td> " + "<b>"+ "Total Marks"  + "</b>" + "</td>"+
+//                 "<td> " + "<b>"+ "obtained Marks"  + "</b>" + "</td>"+
+//                 "<td> " + "<b>"+ "Percentage"  + "</b>" + "</td>")
+// document.write("</tr>")
+
+// document.write("<tr>")
+// document.write("<td> " + sub1  + "</td>" +  
+//                 "<td> "+ totalMark + "</td>"+
+//                 "<td> " +obt1 + "</td>"+
+//                 "<td> "+ (obt1 /totalMark * 100)   +"%"+"</td>")
+// document.write("</tr>")
+
+// document.write("<tr>")
+// document.write("<td> " + sub2  + "</td>" +  
+//                 "<td> "+ totalMark + "</td>"+
+//                 "<td> " +obt2 + "</td>"+
+//                 "<td> "+ (obt2 / totalMark *100) +"%" + "</td>")
+// document.write("</tr>")
+
+
+// document.write("<tr>")
+// document.write("<td> " + sub3  + "</td>" +  
+//                 "<td> "+ totalMark + "</td>"+
+//                 "<td> " +obt3 + "</td>"+
+//                 "<td> "+ (obt3 / totalMark *100) +"%"  + "</td>")
+// document.write("</tr>")
+
+// document.write("<tr>")
+// document.write("<td> "   + "</td>" +  
+//                 "<td> "+ 300 + "</td>"+
+//                 "<td> " +totalmarks + "</td>"+
+//                 "<td> "+ per +"%"  + "</td>")
+// document.write("</tr>")
+// document.write("</table>")
+
+//////////=======XXXXXXXXXXXXX====== END XXXXXXXXXX===========XXXXXX//////////
+
+/////============ CHAPTER NO 09 - 11 ========/////
+
+/////=============Question # 01 ======//////
+// var cityName = prompt("Enter your city Name:") 
+// var lower =  cityName.toLowerCase()
+// if(lower === "karachi"){
+//     alert("Welcome to city of lights")
+// }               
+/////=============Question # 02 ======//////
+
+// var gender = prompt("Enter your Gender:")
+// gender = gender.toLowerCase()
+// if(gender == "male"){
+//     alert("GOOD MORNING SIR!")
+// }
+// else if(gender == "female")
+// {
+//     alert("GOOD MORNING MADAM!")
+// }
+// else{
+//     alert("Enter a correct value")
+// }
+
+
+/////=============Question # 03 ======//////
+
+// var trafficColor = prompt("PLEASE ENTER A TRAFFIC SIGNAL COLOR");
+// trafficColor = trafficColor.toLowerCase();
+// if(trafficColor == "red"){
+//     alert("MUSt STOP")
+// }                
+// else if(trafficColor == "yellow"){
+//     alert("Ready to move")
+// }                
+// if(trafficColor == "green"){
+//     alert("Move NOw")
+// }                
+
+/////=============Question # 04 ======//////
+
+// var fuel = prompt("Please enter a remaining Fuel in car" , "10litres");
+// if(fuel <= "0.25liters"){
+//     alert("Please refill the fuel in your car")
+// }
+
+// else{
+//     alert("Please enter a correct value")
+// }
+
+
+/////=============Question # 05 ======//////
+
+// a.var a = 4;
+// if (++a === 5) {
+//     alert("given condition for variable a is true");
+// }
+
+    //ANSWER // 
+    // Displayed
+
+    // b. var b = 82;
+    // if (b++ === 83){
+    // alert("given condition for variable b is true");
+    // }
+    
+    //ANSWER // 
+    // NOT Displayed
+
+
+
+    
+    //  var c = 12;
+    // if (c++ === 13){
+    // alert("condition 1 is true");
+    // }
+    
+    //ANSWER // 
+    // NOT Displayed
+
+
+
+    // if (c === 13){
+    // alert("condition 2 is true");
+    // }
+
+    //ANSWER // 
+    //  Displayed
+
+    // if (++c < 14){
+    // alert("condition 3 is true");
+    // }
+    //ANSWER // 
+    // NOT Displayed
+
+    // if(c === 14){
+    // alert("condition 4 is true");
+    // }
+    //ANSWER // 
+    //  Displayed
+
+
+
+    // d. var materialCost = 20000;
+    // var laborCost = 2000;
+    // var totalCost = materialCost + laborCost;
+    // if (totalCost === laborCost + materialCost){
+    // alert("The cost equals");
+    // }
+    //ANSWER // 
+    //  Displayed
+
+    // e. if (true){
+    // alert("True");
+    // }
+    // if (false){
+    // alert("False");
+    // }
+    
+    // f. if("car" < "cat"){
+    //     alert("car is smaller than cat");
+    //     }
+        
+    //ANSWER // 
+    //  Displayed
+
+
+        /////=============Question # 06 ======//////
+
+// var sub1 = parseInt(prompt("Enter Subject 1 Mark"));
+// var sub2 = parseInt(prompt("Enter Subject 2 Mark"));
+// var sub3 = parseInt(prompt("Enter Subject 3 Mark"));
+// var totalMarks = parseInt(prompt("Enter Total MArks"));
+// var obtMarks = sub1 + sub2 + sub3 ;
+// var per = obtMarks / totalMarks *100;
+// if(per>=80 && per<=100){
+//     document.write(`Total Marks : ${totalMarks} <br> 
+//                     Marks Obtained : ${obtMarks} <br>
+//                     Percentage : ${per} <br>
+//                     Grade : A-One <br>
+//                     Remarks : Excellent`)
+// }
+// else if(per>=70 && per<=80){
+//     document.write(`Total Marks : ${totalMarks} <br> 
+//                     Marks Obtained : ${obtMarks} <br>
+//                     Percentage : ${per} <br>
+//                     Grade : A <br>
+//                     Remarks : Good`)
+// }
+// else if(per>=60 && per<=70){
+//     document.write(`Total Marks : ${totalMarks} <br> 
+//                     Marks Obtained : ${obtMarks} <br>
+//                     Percentage : ${per} <br>
+//                     Grade : B <br>
+//                     Remarks : You need to improve`)
+// }
+// else if(per>=00 && per<=60){
+//     document.write(`Total Marks : ${totalMarks} <br> 
+//                     Marks Obtained : ${obtMarks} <br>
+//                     Percentage : ${per} <br>
+//                     Grade : Fail <br>
+//                     Remarks : Sorry`)
+// }
+// else{
+//     alert("Please enter a correct Values")
+// }
+
+
+        /////=============Question # 07 ======//////
+
+// var secretNum = 8;
+// var userNum = parseInt(prompt("Enter a scret Number" , "Range 1 to 10"))
+// if(userNum === secretNum){
+//     alert("Bingo! Correct answer")
+// }        
+
+// else if(userNum === ++secretNum){
+//     alert("Close enough to the correct answer")
+// }
+// else{
+//     alert("Please enter a correct number")
+// }
+
+
+        /////=============Question # 08 ======//////
+// var num = parseInt(prompt("Please enter a number"));
+// var divisible = num % 3;
+// if(divisible === 0 ) {
+//     alert("Your Number is divisible by 3")
+// }
+// else{
+//     alert("Your Number is not Divisible by 3")
+// }
+
+        /////=============Question # 09 ======//////
+
+// var number = parseInt(prompt("Please enter a Number"));
+// var even = number % 2 ;
+// if(even === 0){
+//     alert("YOUR NUMBER IS EVEN")
+// }
+// else{
+//     alert("YOUR NUMBER IS ODD")
+// }
+         
+        /////=============Question # 09 ======//////
+
+// var temp = parseInt(prompt("Enter weather Temperation"));
+// if(temp>=40 ){
+//     alert("It is too hot outside. ")
+// }        
+// else if(temp>=30 && temp <=40){
+//     alert("The Weather today is Normal.")
+// }
+// else if(temp>=20 && temp<=30){
+//     alert("Today’s Weather is cool.")
+// }
+// else if(temp>=10 && temp<=20){
+//     alert("OMG! Today’s weather is so Cool.")
+// }
+
+        /////=============Question # 10 ======//////
+
+// var num1  = parseInt(prompt("Enter Number 1"));
+// var opt  = prompt("Enter Operator");
+// var num2  = parseInt(prompt("Enter Number 2"));
+// if(opt === "+" ){
+//     alert( `${num1} + ${num2} = ${(+num1) + (+num2)}` )
+// }
+// else if(opt === "-" ){
+//     alert( `${num1} - ${num2} = ${num1 - num2}` )
+// }
+// else if(opt === "*" ){
+//     alert( `${num1} x ${num2} = ${num1 * num2}` )
+// }
+// else if(opt === "-" ){
+//     alert( `${num1} / ${num2} = ${num1 / num2}` )
+// }
+// else if(opt === "%" ){
+//     alert( `${num1} % ${num2} = ${num1 % num2}` )
+// }
+
+
+    //////xxxxxx======xxxxx END =======xxxxx//////////
+
+    
+
+
 /////============ CHAPTER NO 12 - 13  ========/////
 
 
@@ -433,155 +791,139 @@
 // }
 
 
-/////============ CHAPTER NO 06  -  09 ========/////
 
-/////=============Question # 01 ======//////
+            /////============ CHAPTER NO 14 - 16 ========/////
 
-// var a = 10 ; 
-// document.write(` Result: <br>
-//                 The value of a is : ${a} <br>
-//                 <br> <br>
-//                 .................................... <br> <br>
-//                 The value if ++a is: ${++a} <br>
-//                 Now the value  of a is: ${a}
-//                 <br> <br>
-//                 The value if a++ is: ${a++} <br>
-//                 Now the value  of a is: ${a}
-//                 <br> <br>
-//                 The value if --a is: ${--a} <br>
-//                 Now the value  of a is: ${a}
-//                 <br> <br>
-//                 The value if a-- is: ${a--} <br>
-//                 Now the value  of a is: ${a}
+            /////=============Question # 01 ======//////
 
-//                 `)
+// var stdNames = []
 
 
-/////=============Question # 02 ======//////
-// var a = 2;
-// var b = 1;
-// var result = --a - --b  + ++b + b;
-//             // 1    -  0   +  1  + 1
-// document.write(` a is ${a} <br> 
-//                 b is ${b} <br>
-//                 result is ${result}
-//                 `)
 
+            /////=============Question # 02 ======//////
 
-/////=============Question # 03 ======//////
+// var stdName = new Array()
 
-// var userName = prompt("Enter your name");
-// alert("Hello " + userName)    
+            /////=============Question # 03 ======//////
+// var stringArr = ["Jaffar" , "Aman" , "bilal"]            
 
-/////=============Question # 04 - 05  ======//////
+            /////=============Question # 04 ======//////
+// var numbersArr = [20 ,50 , 100 , 80];
+            
 
-// var tableNum = prompt("please enter a table number");
-// if (tableNum !== "") {
-//     for (var i = 1; i <= 10; i++) {
-//         document.write(`${tableNum} x ${i} = ${tableNum * i} <br>`)
-//     }
+            /////=============Question # 05 ======//////
+// var boolarray = [true , true , false ,true]            
+            
+            /////=============Question # 06 ======//////
+// var mixArray = [50 , "Jaffar" , true];
+
+            /////=============Question # 07 ======//////
+
+// var qualifications = ["SSC", "HSC", "BCS","BS", "BCOM", "MS","M. Phil.", "PhD"]
+// document.write("Qualifications: <br>")            
+// for(var i = 0 ; i<qualifications.length ; i++){
+//     document.write(` <br>
+//                     ${i+1}) ${qualifications[i]}`)
 // }
-// else {
-//     for (var i = 1; i <= 10; i++) {
-//         document.write(`5 x ${i} = ${5 * i} <br>`)
-//     }
+            /////=============Question # 08 ======//////
+
+// var stdNames = ["Jaffar" , "Sir Ali" , "Basit"];
+// var stdMarks = [320 , 230, 480];
+// var per = [stdMarks[0]/500 * 100 , stdMarks[1]/500 * 100 , stdMarks[2]/500 * 100 ]
+// for(var i = 0 ; i<stdMarks.length ; i++){
+//     document.write("Score Of " + " " + stdNames[i] + " " + "is" + stdMarks[0]  + " " + "Percentage: " + per[i] + "%" + "<br>" )
 // }
 
 
-/////=============Question # 06 ======//////
-
-// var sub1 = prompt("Enter subject 1 name");
-// var sub2 = prompt("Enter subject 2 name");
-// var sub3 = prompt("Enter subject 3 name");
-// var totalMark = 100;
-// var obt1 = parseInt(prompt("Enter OBT. Marks subject 1 "))
-// var obt2 = parseInt(prompt("Enter OBT. Marks subject 2 "))
-// var obt3 = parseInt(prompt("Enter OBT. Marks subject 3 "))
-// var totalmarks = obt1 + obt2 + obt3;
-// var per = totalmarks / 300 * 100;
-// // per.toFixed()
-
-// document.write("<table>")
-// document.write("<tr>")
-// document.write("<td> " + "<b>"+ "Subject"  + "</b>" + "</td>" +  
-//                 "<td> " + "<b>"+ "Total Marks"  + "</b>" + "</td>"+
-//                 "<td> " + "<b>"+ "obtained Marks"  + "</b>" + "</td>"+
-//                 "<td> " + "<b>"+ "Percentage"  + "</b>" + "</td>")
-// document.write("</tr>")
-
-// document.write("<tr>")
-// document.write("<td> " + sub1  + "</td>" +  
-//                 "<td> "+ totalMark + "</td>"+
-//                 "<td> " +obt1 + "</td>"+
-//                 "<td> "+ (obt1 /totalMark * 100)   +"%"+"</td>")
-// document.write("</tr>")
-
-// document.write("<tr>")
-// document.write("<td> " + sub2  + "</td>" +  
-//                 "<td> "+ totalMark + "</td>"+
-//                 "<td> " +obt2 + "</td>"+
-//                 "<td> "+ (obt2 / totalMark *100) +"%" + "</td>")
-// document.write("</tr>")
-
-
-// document.write("<tr>")
-// document.write("<td> " + sub3  + "</td>" +  
-//                 "<td> "+ totalMark + "</td>"+
-//                 "<td> " +obt3 + "</td>"+
-//                 "<td> "+ (obt3 / totalMark *100) +"%"  + "</td>")
-// document.write("</tr>")
-
-// document.write("<tr>")
-// document.write("<td> "   + "</td>" +  
-//                 "<td> "+ 300 + "</td>"+
-//                 "<td> " +totalmarks + "</td>"+
-//                 "<td> "+ per +"%"  + "</td>")
-// document.write("</tr>")
-// document.write("</table>")
-
-    //////////=======XXXXXXXXXXXXX====== END XXXXXXXXXX===========XXXXXX//////////
-
-                /////============ CHAPTER NO 09 - 10 ========/////
-
-                /////=============Question # 01 ======//////
-// var cityName = prompt("Enter your city Name:") 
-// var lower =  cityName.toLowerCase()
-// if(lower === "karachi"){
-//     alert("Welcome to city of lights")
-// }               
-                /////=============Question # 02 ======//////
-
-// var gender = prompt("Enter your Gender:")
-// gender = gender.toLowerCase()
-// if(gender == "male"){
-//     alert("GOOD MORNING SIR!")
-// }
-// else if(gender == "female")
-// {
-//     alert("GOOD MORNING MADAM!")
+            /////=============Question # 09 ======//////
+// var colorName = [ "red" , "Blue" , "Purple" , "Black"];
+// document.write (" COLOR Name =>" + " " + colorName + "<br>")
+//Part(A)
+// var addColor = prompt("Add color to the beginning of the array.");
+// colorName.unshift(addColor)
+// document.write(  "ADD COLOR =>" + " "  +colorName)
+ 
+//PART(B)
+// var addColor2 = prompt("Add color to the End of the array.");
+// colorName.push(addColor2)
+// document.write( "<br>" + "ADD COLOR last =>" + " "  +colorName)
+//PART(C)
+// var color1 = prompt("ADD COLOR 1 to the beginning of the Array")
+// var color2 = prompt("ADD COLOR 2 to the beginning of the Array")
+// colorName.unshift(color1 , color2 )
+// document.write(colorName)
+//PART(D)
+// colorName.shift();
+// document.write("Delete First array => " + colorName)
+//PART(E)
+// colorName.pop();
+// document.write("Delete Last array => " + colorName)
+//PART(F)
+// var indNum = parseInt(prompt("Enter a index number "))
+// var colorAdd = (prompt("Enter a colors name "))
+// colorName.splice(indNum, 0 , colorAdd);
+// document.write(colorName)
+//PART (G)
+// var qts = prompt("You want to delete a colors (yes/no)")
+// if(qts === "yes"){
+//     var qts2 = parseInt(prompt("how many color s you want to delete")) 
 // }
 // else{
-//     alert("Enter a correct value")
+//     alert("as your wish")
 // }
-
-
-                /////=============Question # 03 ======//////
-
-// var trafficColor = prompt("PLEASE ENTER A TRAFFIC SIGNAL COLOR");
-// trafficColor = trafficColor.toLowerCase();
-// if(trafficColor == "red"){
-//     alert("MUSt STOP")
-// }                
-// else if(trafficColor == "yellow"){
-//     alert("Ready to move")
-// }                
-// if(trafficColor == "green"){
-//     alert("Move NOw")
-// }                
+// colorName.splice(qts2 , qts2)
+// document.write(colorName)
 
 
 
+            /////=============Question # 10 ======//////
+
+// var stdScore = [320,230,480,120];
+// document.write( "BEFORE SORTING :  " + " " + stdScore + "<br>")
+//  stdScore.sort()
+// document.write( "AFTER SORTING :  " + " " + stdScore)
+
+            /////=============Question # 11 ======//////
+
+// var cities = ["karachi" , "Lahore" , "Islamabad" , "Queeta" , "Peshawar"];
+// var selectedCities = cities.slice(2,4)
+// document.write(`Cities list : <br> ${cities} <br> <br> <br> 
+//                 Selected Cities Name <br> ${selectedCities}`)            
 
 
 
+            /////=============Question # 12 ======//////
+
+// var stringg = ["This" , "is" , "my" , "cat"];
+// var arr= stringg.join(" ")            
+// document.write(arr)
+
+            /////=============Question # 13 ======//////
+
+// var devices = ["keyboard" , "mouse" , "printer" , "monitor"];
+// for(var i = 0 ; i<devices.length;i++){
+//     document.write(`Out: <br>
+//                     ${devices[i]} <br>`)
+// }            
+
+
+            /////=============Question # 14 ======//////
+// var devices = ["keyboard" , "mouse" , "printer" , "monitor"];
+// document.write(`out : <br>
+//                 ${devices[3]} <br>
+//                 out : <br>
+//                 ${devices[2]} <br>
+//                 out : <br>
+//                 ${devices[1]} <br>
+//                 out : <br>
+//                 ${devices[0]} <br>`)
+            
+            /////=============Question # 15 ======//////
+
+// var mobiles = ["Apple","Samsung", "Motorola", "Nokia", "Sony" , "Haier"];
+// document.write("<select>")
+// for(var i=0;i<mobiles.length;i++){
+//     document.write("<option>" + mobiles[i]  +"</option>")
+// }            
+// document.write("</select>")            
 
